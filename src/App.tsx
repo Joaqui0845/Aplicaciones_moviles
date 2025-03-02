f4879f4011e043013bca78d15ae8064ad22e8850
Your Name <you@example.com>
2025-03-01 19:03:19 -0500

    Primer commit - subiendo proyecto utilizando react y vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          <h1>¡Hola, mundo! 🌍</h1>
+          <p>Bienvenido a mi aplicación de nutrición.</p>
+      </div>
+  );
+}
+
+export default App;
